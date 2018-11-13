010ab3d67dc095022e7b66ba2bed9529dc953178
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-11-13 15:39:16 +0000

    aural-status tests complete

---
+++ src/components/tests/aural-status.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import { expect } from 'chai';
+
+
+import AuralStatus from '../aural-status';
+
+describe('<AuralStatus />', () => {
+
+  it('Renders without crashing', () => {
+    shallow(<AuralStatus />);
+  });
+  
+  it('Renders a paragraph with class `visuallyhidden`', () => {
+    const wrapper = shallow(<AuralStatus />);
+    expect(wrapper.is('p.visuallyhidden')).to.equal(true);
+  });
+  
+  it('Should render prop auralStatus as its content', () => {
+    const value = "foo";
+    const wrapper = mount(
+      <AuralStatus auralStatus={value} />);
+    wrapper.update();
+    const contents = wrapper.text();
+    expect(contents).to.equal(value);
+  });
+  
+  
+});
