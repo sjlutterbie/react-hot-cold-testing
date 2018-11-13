2035c1227b6d4b0392d0b9c2d74b8b5587c260df
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-11-13 16:34:25 +0000

    guess-list tests complete

---
+++ src/components/tests/guess-list.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { expect } from 'chai';
+
+import GuessList from '../guess-list';
+
+describe('<GuessList />', () => {
+  
+  it('Should render without crashing', () => {
+    // NOTE: This element requires prop guesses to pass shallow testing.
+    const guesses = ['a','b','c'];
+    shallow(<GuessList guesses={guesses}/>);
+  });
+
+  it('Should render an ul element', () => {
+    const guesses = ['a','b','c'];
+    const wrapper = shallow(<GuessList guesses={guesses} />);
+    expect(wrapper.is('ul')).to.equal(true);
+  });
+  
+  it('Should contain the correct length list', () => {
+    const guesses = ['a','b','c'];
+    const wrapper = shallow(<GuessList guesses={guesses} />);
+    expect(wrapper.find('ul').children()).to.have.length(guesses.length);
+  });
+});
