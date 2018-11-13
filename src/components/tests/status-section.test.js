a8893413132e65c10837f8a4f142be2ad24fcbd8
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-11-13 17:17:08 +0000

    status-section tests complete

---
+++ src/components/tests/status-section.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import {expect} from 'chai';
+
+import StatusSection from '../status-section';
+
+describe('<StatusSection />', () => {
+  
+  it('Should render without crashing', () => {
+    // NOTE: Requires guesses prop to pass shallow rendering
+    const guesses = ['a','b','c'];
+    shallow(<StatusSection guesses={guesses} />);
+  });
+  
+  it('Should render a <section> element', () => {
+    const guesses = ['a','b','c'];
+    const wrapper = shallow(<StatusSection guesses={guesses} />);
+    expect(wrapper.is('section')).to.equal(true);
+  });
+  
+});
